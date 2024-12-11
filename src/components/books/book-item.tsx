847c075b1068bd2bcd35ae5c7e86d121d98378eb
KXSXJ <100739107+KXSXJ@users.noreply.github.com>
2024-12-11 22:27:25 +0900

    fix: Modify Date Type - type: Date => String

--- src/components/books/book-item.tsx
+++ src/components/books/book-item.tsx
@@ -12,7 +12,7 @@ export default function BookItem({book}:BookItemProps){
                 <Image src={`${book.image_url}`} alt='book_img' width={100} height={100} loading="lazy"/>
                 <div>
                     <h4>{book.title}</h4>
-                    <p>{book.author} &#183; {formatDate(book.published_date)}</p>
+                    <p>{book.author} &#183; {formatDate(new Date(book.published_date))}</p>
                     <p>{book.price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}원</p>
                     <p>판매수량 : {book.sales_quantity}권</p>
                 </div>
